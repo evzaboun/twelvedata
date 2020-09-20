1934e6490c404130f106ec27abff5429b7c37a86
vagelis z <29361890+evzab@users.noreply.github.com>
2020-09-20 21:02:10 +0300

    adding gitignore and axios singleton

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-import Earnings from './lib/earnings';
-import TimeSeries from './lib/timeSeries';
+import Earnings from "./lib/earnings";
+import TimeSeries from "./lib/timeSeries";
 
 export default (config = {}) => {
   // Check for API Key
   if (config.key === undefined) {
-    throw new Error('Missing config settings: key');
+    throw new Error("Missing config settings: key");
   }
 
   const availableEndpoints = {
-    earnings: 'earnings',
-    time_series: 'time_series',
-    complex_data: 'complex_data',
-    stocks: 'stocks',
+    earnings: "earnings",
+    time_series: "time_series",
+    complex_data: "complex_data",
+    stocks: "stocks",
   };
 
   // Base url for the client
