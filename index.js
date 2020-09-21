d21b1485910871876d75b4a554ce2a3f2cf93464
zaabalonso <zaabalonso@gmail.com>
2020-09-20 22:24:44 -0700

    refactoring the modules and extracting the utils methods to separate file

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ module.exports = (config) => {
   if (config.key === undefined) {
     throw new Error("Missing config settings: key");
   }
-  // Base url for the client
-  config.base = `https://api.twelvedata.com/`;
-  config.availableEndpoints = {
-    earnings: "earnings",
-    time_series: "time_series",
-    complex_data: "complex_data",
-    stocks: "stocks",
-  };
 
   return {
     earnings: new Earnings(config),
