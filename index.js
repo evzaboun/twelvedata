b6883ff947506865ba5584963a3bbe6fcf32dd7f
zaabalonso <zaabalonso@gmail.com>
2020-10-04 21:30:40 -0700

    adding the rest of the api methods

--- index.js
+++ index.js
@@ -19,13 +19,22 @@ module.exports = (config) => {
   const utilities = new utils(config.key);
 
   return {
-    earnings: utilities.createFunc("earnings", requestTypes.get),
-    timeSeries: utilities.createFunc("timeSeries", requestTypes.get),
     apiUsage: utilities.createFunc("apiUsage", requestTypes.get),
-    stocks: utilities.createFunc("stocks", requestTypes.get),
-    price: utilities.createFunc("price", requestTypes.get),
-    earningsCalendar: utilities.createFunc("earningsCalendar", requestTypes.get),
-    quote: utilities.createFunc('quote', requestTypes.get),
     complexData: utilities.createFunc("complexData", requestTypes.post),
+    cryptocurrencies: utilities.createFunc("cryptocurrencies", requestTypes.get),
+    cryptocurrencyExchanges: utilities.createFunc("cryptocurrencyExchanges", requestTypes.get),
+    earliestTimestamp: utilities.createFunc("earliestTimestamp", requestTypes.get),
+    earnings: utilities.createFunc("earnings", requestTypes.get),
+    earningsCalendar: utilities.createFunc("earningsCalendar", requestTypes.get),
+    etf: utilities.createFunc("etf", requestTypes.get),
+    exchanges: utilities.createFunc("exchanges", requestTypes.get),
+    forexPairs: utilities.createFunc("forexPairs", requestTypes.get),
+    indices: utilities.createFunc("indices", requestTypes.get),
+    price: utilities.createFunc("price", requestTypes.get),
+    quote: utilities.createFunc('quote', requestTypes.get),
+    stocks: utilities.createFunc("stocks", requestTypes.get),
+    symbolSearch: utilities.createFunc("symbolSearch", requestTypes.get),
+    technicalIndicators: utilities.createFunc("technicalIndicators", requestTypes.get),
+    timeSeries: utilities.createFunc("timeSeries", requestTypes.get)
   };
 };
