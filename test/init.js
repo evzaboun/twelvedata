1c7a2030f7e03ca74ca72aa62217fb656471e2d6
zaabalonso <zaabalonso@gmail.com>
2020-10-06 22:40:32 -0700

    completing index tests

--- test/init.js
+++ test/init.js
@@ -12,6 +12,21 @@ test(`initialization with a config is succesfull`, () => {
   const twelveData = TwelveData({key: 'a-key'});
 
   expect(twelveData).toBeDefined();
+  expect(twelveData.apiUsage).toBeDefined();
+  expect(twelveData.complexData).toBeDefined();
+  expect(twelveData.cryptocurrencies).toBeDefined();
+  expect(twelveData.cryptocurrencyExchanges).toBeDefined();
+  expect(twelveData.earliestTimestamp).toBeDefined();
   expect(twelveData.earnings).toBeDefined();
+  expect(twelveData.earningsCalendar).toBeDefined();
+  expect(twelveData.etf).toBeDefined();
+  expect(twelveData.exchanges).toBeDefined();
+  expect(twelveData.forexPairs).toBeDefined();
+  expect(twelveData.indices).toBeDefined();
+  expect(twelveData.price).toBeDefined();
+  expect(twelveData.quote).toBeDefined();
+  expect(twelveData.stocks).toBeDefined();
+  expect(twelveData.symbolSearch).toBeDefined();
+  expect(twelveData.technicalIndicators).toBeDefined();
   expect(twelveData.timeSeries).toBeDefined();
 });
