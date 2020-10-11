2a0ac23b412526387bf6484796366b33c3a41e79
zaabalonso <zaabalonso@gmail.com>
2020-10-11 13:09:02 -0700

    tests refactoring

--- test/init.test.js
+++ test/init.test.js
@@ -1,34 +1,36 @@
 import TwelveData from '..';
 
-test(`initialization without a config throws an error`, () => {
-  expect(() => TwelveData()).toThrow(`Missing config`);
-});
-
-test(`initialization without a config key throws an error`, () => {
-  expect(() => TwelveData({})).toThrow(`Missing config settings: key`);
-});
-
-test(`initialization with a config is succesfull`, () => {
-  const twelveData = TwelveData({
-    key: 'a-key'
+describe("index", () => {
+  test(`initialization without a config throws an error`, () => {
+    expect(() => TwelveData()).toThrow(`Missing config`);
   });
 
-  expect(twelveData).toBeDefined();
-  expect(twelveData.apiUsage).toBeDefined();
-  expect(twelveData.complexData).toBeDefined();
-  expect(twelveData.cryptocurrencies).toBeDefined();
-  expect(twelveData.cryptocurrencyExchanges).toBeDefined();
-  expect(twelveData.earliestTimestamp).toBeDefined();
-  expect(twelveData.earnings).toBeDefined();
-  expect(twelveData.earningsCalendar).toBeDefined();
-  expect(twelveData.etf).toBeDefined();
-  expect(twelveData.exchanges).toBeDefined();
-  expect(twelveData.forexPairs).toBeDefined();
-  expect(twelveData.indices).toBeDefined();
-  expect(twelveData.price).toBeDefined();
-  expect(twelveData.quote).toBeDefined();
-  expect(twelveData.stocks).toBeDefined();
-  expect(twelveData.symbolSearch).toBeDefined();
-  expect(twelveData.technicalIndicators).toBeDefined();
-  expect(twelveData.timeSeries).toBeDefined();
+  test(`initialization without a config key throws an error`, () => {
+    expect(() => TwelveData({})).toThrow(`Missing config settings: key`);
+  });
+
+  test(`initialization with a config is succesfull`, () => {
+    const twelveData = TwelveData({
+      key: 'a-key'
+    });
+
+    expect(twelveData).toBeDefined();
+    expect(twelveData.apiUsage).toBeDefined();
+    expect(twelveData.complexData).toBeDefined();
+    expect(twelveData.cryptocurrencies).toBeDefined();
+    expect(twelveData.cryptocurrencyExchanges).toBeDefined();
+    expect(twelveData.earliestTimestamp).toBeDefined();
+    expect(twelveData.earnings).toBeDefined();
+    expect(twelveData.earningsCalendar).toBeDefined();
+    expect(twelveData.etf).toBeDefined();
+    expect(twelveData.exchanges).toBeDefined();
+    expect(twelveData.forexPairs).toBeDefined();
+    expect(twelveData.indices).toBeDefined();
+    expect(twelveData.price).toBeDefined();
+    expect(twelveData.quote).toBeDefined();
+    expect(twelveData.stocks).toBeDefined();
+    expect(twelveData.symbolSearch).toBeDefined();
+    expect(twelveData.technicalIndicators).toBeDefined();
+    expect(twelveData.timeSeries).toBeDefined();
+  });
 });
