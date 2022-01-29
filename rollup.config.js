e798d77f08ec103d4bcdf4bf3169a4eba7ae94b3
zaabalonso <zaabalonso@gmail.com>
2022-01-28 23:21:57 -0800

    building for both node/es6 modules using rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,73 @@
 import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
 import json from "rollup-plugin-json";
-
+import nodeResolve from "@rollup/plugin-node-resolve";
+import { readFileSync, writeFileSync } from "fs";
 import pkg from "./package.json";
 
+let numberOfBundlesBuilt = 0;
+let outputBundles = [
+  {
+    file: "./dist/dist/twelvedata.js",
+    format: "umd",
+    name: pkg.name,
+  },
+  {
+    file: "./dist/dist-esm/twelvedata.js",
+    format: "es",
+  },
+];
+const externalDependencies = ["axios"];
+
+export function initRollupPostScript() {
+  try {
+    // Read internal packagejson and create public package json
+    const internalPackageJson = JSON.parse(readFileSync(`./package.json`));
+
+    delete internalPackageJson.scripts;
+    delete internalPackageJson.devDependencies;
+    delete internalPackageJson.jest;
+    delete internalPackageJson.main;
+    delete internalPackageJson.files;
+    delete internalPackageJson.source;
+
+    Object.keys(internalPackageJson.dependencies).forEach((dep) => {
+      if (externalDependencies.indexOf(dep) < 0) {
+        delete internalPackageJson.dependencies[dep];
+      }
+    });
+    writeFileSync(
+      `./dist/package.json`,
+      JSON.stringify(internalPackageJson, null, 2)
+    );
+  } catch (Error) {
+    console.error("Post script failed", Error);
+  }
+}
 export default {
   input: pkg.source,
-  output: [
-    {
-      file: pkg.main,
-      format: "umd",
-      name: pkg.name,
-    }
-  ],
+  output: outputBundles,
   external: [
     ...Object.keys(pkg.devDependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {})
+    ...Object.keys(pkg.peerDependencies || {}),
   ],
 
   plugins: [
     json(),
-    resolve(),
-    commonjs()
-  ]
+    nodeResolve({
+      mainFields: ["module", "browser"],
+      preferBuiltins: false,
+    }),
+    commonjs(),
+    {
+      name: "buildHooks",
+      writeBundle() {
+        numberOfBundlesBuilt++;
+        if (numberOfBundlesBuilt === outputBundles.length) {
+          console.log("Done with Bundles. Running post script");
+          initRollupPostScript();
+          numberOfBundlesBuilt = 0;
+        }
+      },
+    },
+  ],
 };
