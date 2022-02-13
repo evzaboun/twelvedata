05e7db95d0c7dcd51496420130c41d8b53468cfc
zaabalonso <zaabalonso@gmail.com>
2022-02-13 09:13:56 -0800

    fixing e2e requests on node/browser/react

--- examples/app.js
+++ examples/app.js
@@ -1,27 +1,26 @@
-const twelvedata = require('../dist/twelvedata.js');	
+import twelvedata from "../dist/dist-esm/twelvedata.js";
 
-// setup the key	
-const config = {	
-  key: 'your-api-key'	
-};	
-
-// init the client	
-const client = twelvedata(config);	
-	
-const paramsPost = {	
-  symbols: ["AAPL", "MSFT", "GOOG"],	
-  intervals: ["5min", "1day"],	
-  outputsize: 5,	
-  methods: [	
-    "time_series",	
-    {	
-      name: "ema",	
-      time_period: 12,	
-    },	
-  ],	
+// setup the key
+const config = {
+  key: "your-api-key",
 };
 
-client.complexData(paramsPost).then((data) => {	
-  console.log(data);	
-});	
+// init the client
+const client = twelvedata(config);
 
+const paramsPost = {
+  symbols: ["AAPL", "MSFT", "GOOG"],
+  intervals: ["5min", "1day"],
+  outputsize: 5,
+  methods: [
+    "time_series",
+    {
+      name: "ema",
+      time_period: 12,
+    },
+  ],
+};
+
+client.complexData(paramsPost).then((data) => {
+  console.log(data);
+});
